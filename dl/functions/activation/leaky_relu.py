8876247479f83d14c72de21fdaa5822463884a19
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2022-07-28 20:22:15 +0100

    update name class of leaky relu activation function from Leaky_ReLU to LeakyReLU

--- dl/functions/activation/leaky_relu.py
+++ dl/functions/activation/leaky_relu.py
@@ -3,11 +3,11 @@ import numpy as np
 from dl.functions import Function
 
 
-class Leaky_ReLU(Function):
+class LeakyReLU(Function):
     a = 0.01
 
     def __call__(self, Z):
-        return np.maximum(Leaky_ReLU.a * Z, Z)
+        return np.maximum(LeakyReLU.a * Z, Z)
 
     def prime(self, Z):
-        return np.where(Z > 0, 1, Leaky_ReLU.a)
+        return np.where(Z > 0, 1, LeakyReLU.a)
