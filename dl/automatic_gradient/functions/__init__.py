5bfa516847191be6591346a9b0b8d02a8f2a2e61
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2022-12-30 22:44:30 +0100

    ADD: done adding linear function to dl/automatic_gradient/functions/__init__.py

--- dl/automatic_gradient/functions/__init__.py
+++ dl/automatic_gradient/functions/__init__.py
@@ -5,6 +5,7 @@ from dl.automatic_gradient.functions.relu import ReLU
 from dl.automatic_gradient.functions.logarithm import Log
 from dl.automatic_gradient.functions.sigmoid import Sigmoid
 from dl.automatic_gradient.functions.tanh import TanH
+from dl.automatic_gradient.functions.linear import Linear
 
 # N-ARY FUNCTIONS
 from dl.automatic_gradient.functions.softmax import Softmax
