2283af54ab2e799b51bd8fd1b4e60e58037adfb0
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2022-12-30 23:27:46 +0100

    ADD: done implementing MAE

--- dl/functions/loss/mae.py
+++ dl/functions/loss/mae.py
@@ -1,8 +1,9 @@
 from dl.functions.loss.loss_function import LossFunction
+from dl.automatic_gradient.functions.absolute import Absolute
 
 class MSE(LossFunction):
     def __init__(self, regularizer=None):
         self.regularizer = regularizer
         
     def __call__(self, y_pred, y):
-        pass
+        return Absolute.run(y_pred - y).sum() / len(y)
