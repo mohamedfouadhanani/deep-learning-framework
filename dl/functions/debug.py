3d0f5d2f30b6d1ec85e0c86247fb7ca036c64b7d
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2022-12-31 18:23:07 +0100

    ADD: adding file debug.py for debggin only

--- dl/functions/debug.py
+++ dl/functions/debug.py
@@ -6,20 +6,16 @@ sys.path.append(os.getcwd())
 
 from dl.automatic_gradient import Variable
 from dl.functions.loss import CategoricalCrossEntropy
+from dl.functions.loss import BinaryCrossEntropy
 
 def main():
-    y = np.array([
-        [1, 0, 0],
-        [0, 1, 0]
-    ])
-
+    y = np.array([0, 1])
     y = Variable.from_numpy(y)
 
-    y_pred = np.random.uniform(0, 1, size=(2, 3))
-
+    y_pred = np.random.uniform(0, 1, size=(2, ))
     y_pred = Variable.from_numpy(y_pred)
 
-    loss = CategoricalCrossEntropy()
+    loss = BinaryCrossEntropy()
     print(loss(y_pred, y))
 
 if __name__ == "__main__":
