a184ae0265ae93c2bcf91e0bcbd20c957ded6102
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2022-12-31 22:42:27 +0100

    ADD: __call__ method for a Dense layer

--- dl/layers/dense.py
+++ dl/layers/dense.py
@@ -7,6 +7,8 @@ class Dense:
     def __init__(self, n_units, activation_function=Linear):
         self.n_units = n_units
         self.activation_function = activation_function
+        self.W = None
+        self.b = None
 
     def initialize(self, n_inputs):
         self.n_inputs = n_inputs
@@ -16,6 +18,12 @@ class Dense:
 
         self.W = Variable.from_numpy(W)
         self.b = Variable.from_numpy(b)
+
+    def __call__(self, A):
+        self.Z = np.dot(A, self.W.T) + self.b
+        self.A = self.activation_function.run(self.Z)
+
+        return self.A
 
     def __repr__(self):
         return f"Dense(n_units={self.n_units}, activation_function={self.activation_function.__name__})"
