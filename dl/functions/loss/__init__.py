770d78d69782e2811c78441f53151080ceaf6804
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2022-12-31 18:22:02 +0100

    FIX: done updating imports

--- dl/functions/loss/__init__.py
+++ dl/functions/loss/__init__.py
@@ -1,3 +1,4 @@
 from dl.functions.loss.mse import MSE
 from dl.functions.loss.mae import MAE
 from dl.functions.loss.categorical_crossentropy import CategoricalCrossEntropy
+from dl.functions.loss.binary_crossentropy import BinaryCrossEntropy
