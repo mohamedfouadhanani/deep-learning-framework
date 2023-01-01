2c1eb9637ab65fa4ce744d450a54b67a3179bc46
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2023-01-01 10:44:49 +0100

    FIX: fixing import in regularization

--- dl/regularization/__init__.py
+++ dl/regularization/__init__.py
@@ -1,3 +1,3 @@
-from .dropout import Dropout
-from .l2 import L2
-from .l1 import L1
+from dl.regularization.l1 import L1
+from dl.regularization.l2 import L2
+from dl.regularization.dropout import Dropout
