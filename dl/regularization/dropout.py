ace7ac5dcc0efe6c9ab3d5caa6714d11e40a80c6
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2023-01-01 10:28:43 +0100

    ADD: dropout layer

--- dl/regularization/dropout.py
+++ dl/regularization/dropout.py
@@ -4,10 +4,19 @@ class Dropout:
     def __init__(self, keep_prob):
         self.keep_prob = keep_prob
     
-    def __call__(self, A):
-        d = np.random.randn(*A.shape)
-        d = (d > self.keep_prob).astype(int)
-        A = d * A
+    def __call__(self, A, **kwargs):
+        is_optimizing = kwargs["is_optimizing"]
+
+        if not is_optimizing:
+            return A
+        
+        d = np.random.uniform(0, 1, size=A.shape)
+        d = (d < self.keep_prob).astype(int)
+
+        A = A * d
         A /= self.keep_prob
 
         return A
+    
+    def __repr__(self):
+        return f"Dropout(keep_prob={self.keep_prob})"
