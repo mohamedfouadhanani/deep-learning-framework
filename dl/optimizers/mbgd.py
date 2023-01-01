477f7dc62534dfc85cf2df5a72088cfa92ba13a6
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2023-01-01 22:35:22 +0100

    ADD: done implementing & testing mini batch gradient descent

--- dl/optimizers/mbgd.py
+++ dl/optimizers/mbgd.py
@@ -1,7 +1,9 @@
 import numpy as np
+from decimal import Decimal
 
+from dl.optimizers.optimizer import Optimizer
 
-class MiniBatchGradientDescent:
+class MiniBatchGradientDescent(Optimizer):
     def __init__(self, batch_size, learning_rate, lr_decay=lambda lr0, epoch: lr0):
         self.batch_size = batch_size
         self.learning_rate0 = learning_rate
@@ -13,48 +15,54 @@ class MiniBatchGradientDescent:
 
     def __call__(self, X, y, n_epochs, verbose=True):
         history = {"loss": []}
-        n, m = X.shape
+
+        m, n = X.shape
         number_batches = np.ceil(m / self.batch_size).astype(int)
 
         for epoch in range(n_epochs):
             # applying learning rate decay
             self.learning_rate = self.lr_decay(self.learning_rate0, epoch)
 
+            average_loss = 0
             for t in range(number_batches):
                 starting_index = t * self.batch_size
-                finishing_index = starting_index + self.batch_size
 
+                finishing_index = starting_index + self.batch_size
                 finishing_index = min(finishing_index, m)
 
-                X_t = X[:, starting_index:finishing_index]
-                y_t = y[:, starting_index:finishing_index]
+                X_t = X[starting_index:finishing_index, :]
+                y_t = y[starting_index:finishing_index, :]
 
                 # forward propagation
-                self.model.layers[0].A = X_t
-                self.model.forward_propagation()
+                y_pred = self.model(X_t, is_optimizing=True)
 
                 # loss calculation
-                y_hat = self.model.layers[-1].A
-                loss = self.model.loss(y_hat, y_t)
+                loss = self.model.loss(y_pred, y_t)
 
-                if self.model.loss.regularizer is not None:
-                    for l in range(1, self.model.L):
-                        loss += self.model.loss.regularizer(m, self.model.layers[l].W)
+                # model params
+                params = self.model.parameters()
 
                 # backward propagation - computing dWi & dbi for every layer
-                self.model.layers[-1].dA = self.model.loss.prime(y_hat, y_t)
-                self.model.backward_propagation(m)
+                self.zero_gradients(params)
+                loss.backward()
 
                 # updating dWi & dbi
-                for l in range(1, self.model.L):
-                    self.model.layers[l].W -= self.learning_rate * self.model.layers[l].dW
-                    self.model.layers[l].b -= self.learning_rate * self.model.layers[l].db
+                for param in params:
+                    self.step(param)
 
                 # printing to the console
                 if verbose:
-                    print(f"[{epoch + 1}/{n_epochs}, {t + 1}/{number_batches}]: loss = {loss}")
+                    print(f"[{epoch + 1}/{n_epochs}, {t + 1}/{number_batches}]: loss = {loss.data}")
+                
+                average_loss += loss.data
 
             # keeping history
-            history["loss"].append(loss)
+            history["loss"].append(average_loss / number_batches)
 
         return history
+    
+    def step(self, variable):
+        variable.data -= Decimal(self.learning_rate) * variable.gradient
+
+    def __repr__(self) -> str:
+        return f"MiniBatchGradientDescent(batch_size={self.batch_size}, learning_rate={self.learning_rate0})"
