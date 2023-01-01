41e02ca1e6bc2efa0ee3de7404b0cafcabef2a7a
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2023-01-01 11:24:21 +0100

    REMOVE: removing intialize method from dropout

--- dl/regularization/dropout.py
+++ dl/regularization/dropout.py
@@ -3,13 +3,13 @@ import numpy as np
 class Dropout:
     def __init__(self, keep_prob):
         self.keep_prob = keep_prob
-    
+
     def __call__(self, A, **kwargs):
         is_optimizing = kwargs["is_optimizing"]
 
         if not is_optimizing:
             return A
-        
+
         d = np.random.uniform(0, 1, size=A.shape)
         d = (d < self.keep_prob).astype(int)
 
@@ -17,9 +17,6 @@ class Dropout:
         A /= self.keep_prob
 
         return A
-    
-    def initialize(self, **kwargs):
-        pass
-    
+
     def __repr__(self):
         return f"Dropout(keep_prob={self.keep_prob})"
