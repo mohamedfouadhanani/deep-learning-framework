d024527706445a3387445efdf6960da80269e2bf
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2023-01-01 22:16:09 +0100

    UPDATE: stochastic gradient descent is implemented & tested

--- dl/optimizers/sgd.py
+++ dl/optimizers/sgd.py
@@ -1,7 +1,8 @@
-import numpy as np
+from decimal import Decimal
 
+from dl.optimizers.optimizer import Optimizer
 
-class StochasticGradientDescent:
+class StochasticGradientDescent(Optimizer):
     def __init__(self, learning_rate, lr_decay=lambda lr0, epoch: lr0):
         self.learning_rate0 = learning_rate
         self.learning_rate = learning_rate
@@ -12,36 +13,32 @@ class StochasticGradientDescent:
 
     def __call__(self, X, y, n_epochs, verbose=True):
         history = {"loss": []}
-        n, m = X.shape
+        m, n = X.shape
 
         for epoch in range(n_epochs):
             # applying learning rate decay
             self.learning_rate = self.lr_decay(self.learning_rate0, epoch)
 
             for i in range(m):
-                X_i = X[:, i].reshape(-1, 1)
-                y_i = y[:, i].reshape(-1, 1)
+                X_i = X[i, :].reshape(-1, 1)
+                y_i = y[i, :].reshape(-1, 1)
 
                 # forward propagation
-                self.model.layers[0].A = X_i
-                self.model.forward_propagation()
+                y_pred = self.model(X_i, is_optimizing=True)
 
                 # loss calculation
-                y_hat = self.model.layers[-1].A
-                loss = self.model.loss(y_hat, y_i)
+                loss = self.model.loss(y_pred, y_i)
 
-                if self.model.loss.regularizer is not None:
-                    for l in range(1, self.model.L):
-                        loss += self.model.loss.regularizer(m, self.model.layers[l].W)
+                # model params
+                params = self.model.parameters()
 
                 # backward propagation - computing dWi & dbi for every layer
-                self.model.layers[-1].dA = self.model.loss.prime(y_hat, y_i)
-                self.model.backward_propagation(m)
+                self.zero_gradients(params)
+                loss.backward()
 
                 # updating dWi & dbi
-                for l in range(1, self.model.L):
-                    self.model.layers[l].W -= self.learning_rate * self.model.layers[l].dW
-                    self.model.layers[l].b -= self.learning_rate * self.model.layers[l].db
+                for param in params:
+                    self.step(param)
 
                 # printing to the console
                 if verbose:
@@ -51,3 +48,9 @@ class StochasticGradientDescent:
             history["loss"].append(loss)
 
         return history
+
+    def step(self, variable):
+        variable.data -= Decimal(self.learning_rate) * variable.gradient
+
+    def __repr__(self) -> str:
+        return f"StochasticGradientDescent(learning_rate={self.learning_rate0})"
