b3b64a40f292ffe7afb89a1af978d5b75958e10d
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2023-01-05 18:11:30 +0100

    ADD: implementing adaptive moment estimation

--- dl/optimizers/adam.py
+++ dl/optimizers/adam.py
@@ -1,7 +1,10 @@
 import numpy as np
+from decimal import Decimal
 
+from dl.layers.dense import Dense
+from dl.optimizers.optimizer import Optimizer
 
-class AdaptiveMomentEstimation:
+class AdaptiveMomentEstimation(Optimizer):
     EPSILON = 1e-7
 
     def __init__(self, batch_size, learning_rate, lr_decay=lambda lr0, epoch: lr0, beta1=0.9, beta2=0.999):
@@ -13,85 +16,98 @@ class AdaptiveMomentEstimation:
         self.beta2 = beta2
 
     def initialize(self):
-        for l in range(1, self.model.L):
-            # velocity
-            self.model.layers[l].vdW = np.zeros((self.model.layers[l].n_units, self.model.layers[l].n_inputs))
-            self.model.layers[l].vdb = np.zeros((self.model.layers[l].n_units, 1))
+        for layer in self.model.layers:
+            if isinstance(layer, Dense):
+                # velocity
+                layer.vdW = np.zeros(layer.W.shape)
+                layer.vdb = np.zeros(layer.b.shape)
+                
+                # squares
+                layer.sdW = np.zeros(layer.W.shape)
+                layer.sdb = np.zeros(layer.b.shape)
 
-            # squares
-            self.model.layers[l].sdW = np.zeros((self.model.layers[l].n_units, self.model.layers[l].n_inputs))
-            self.model.layers[l].sdb = np.zeros((self.model.layers[l].n_units, 1))
+                # velocity corrected
+                layer.vdW_corrected = np.zeros(layer.W.shape)
+                layer.vdb_corrected = np.zeros(layer.b.shape)
 
-            # velocity corrected
-            self.model.layers[l].vdW_corrected = None
-            self.model.layers[l].vdb_corrected = None
-
-            # squares corrected
-            self.model.layers[l].sdW_corrected = None
-            self.model.layers[l].sdb_corrected = None
+                # squares corrected
+                layer.sdW_corrected = np.zeros(layer.W.shape)
+                layer.sdb_corrected = np.zeros(layer.b.shape)
 
     def __call__(self, X, y, n_epochs, verbose=True):
         history = {"loss": []}
-        n, m = X.shape
+
+        m, n = X.shape
         number_batches = np.ceil(m / self.batch_size).astype(int)
 
         for epoch in range(n_epochs):
             # applying learning rate decay
             self.learning_rate = self.lr_decay(self.learning_rate0, epoch)
 
+            average_loss = 0
             for t in range(number_batches):
                 starting_index = t * self.batch_size
                 finishing_index = starting_index + self.batch_size
 
                 finishing_index = min(finishing_index, m)
 
-                X_t = X[:, starting_index:finishing_index]
-                y_t = y[:, starting_index:finishing_index]
+                X_t = X[starting_index:finishing_index, :]
+                y_t = y[starting_index:finishing_index, :]
 
                 # forward propagation
-                self.model.layers[0].A = X_t
-                self.model.forward_propagation()
+                y_pred = self.model(X_t, is_optimizing=True)
 
                 # loss calculation
-                y_hat = self.model.layers[-1].A
-                loss = self.model.loss(y_hat, y_t)
+                loss = self.model.loss(y_pred, y_t)
+
+                # model params
+                params = self.model.parameters()
 
                 # backward propagation - computing dWi & dbi for every layer
-                self.model.layers[-1].dA = self.model.loss.prime(y_hat, y_t)
-                self.model.backward_propagation(m)
+                self.zero_gradients(params)
+                loss.backward()
 
                 # updating dWi & dbi
-                for l in range(1, self.model.L):
-                    # velocity update
-                    self.model.layers[l].vdW = self.beta1 * self.model.layers[l].vdW + \
-                        (1 - self.beta1) * self.model.layers[l].dW
-                    self.model.layers[l].vdb = self.beta1 * self.model.layers[l].vdb + \
-                        (1 - self.beta1) * self.model.layers[l].db
-
-                    # squres update
-                    self.model.layers[l].sdW = self.beta2 * self.model.layers[l].sdW + \
-                        (1 - self.beta2) * self.model.layers[l].dW ** 2
-                    self.model.layers[l].sdb = self.beta2 * self.model.layers[l].sdb + \
-                        (1 - self.beta2) * self.model.layers[l].db ** 2
-
-                    # velocity correction
-                    self.model.layers[l].vdW_corrected = self.model.layers[l].vdW / (1 - self.beta1 ** (t + 1))
-                    self.model.layers[l].vdb_corrected = self.model.layers[l].vdb / (1 - self.beta1 ** (t + 1))
-
-                    # squares correction
-                    self.model.layers[l].sdW_corrected = self.model.layers[l].sdW / (1 - self.beta2 ** (t + 1))
-                    self.model.layers[l].sdb_corrected = self.model.layers[l].sdb / (1 - self.beta2 ** (t + 1))
-
-                    self.model.layers[l].W -= self.learning_rate * self.model.layers[l].vdW_corrected / \
-                        np.sqrt(self.model.layers[l].sdW + AdaptiveMomentEstimation.EPSILON)
-                    self.model.layers[l].b -= self.learning_rate * self.model.layers[l].vdb_corrected / \
-                        np.sqrt(self.model.layers[l].sdb + AdaptiveMomentEstimation.EPSILON)
+                self.step(t)
 
                 # printing to the console
                 if verbose:
-                    print(f"[{epoch + 1}/{n_epochs}, {t + 1}/{number_batches}]: loss = {loss}")
+                    print(f"[{epoch + 1}/{n_epochs}, {t + 1}/{number_batches}]: loss = {loss.data}")
+                
+                average_loss += loss.data
 
             # keeping history
-            history["loss"].append(loss)
+            history["loss"].append(average_loss / number_batches)
 
         return history
+
+    def step(self, t):
+        for layer in self.model.layers:
+            if not isinstance(layer, Dense):
+                continue
+            
+            # updating vdW
+            # updating vdW
+            n_units, n_inputs = layer.vdW.shape
+            for i in range(n_units):
+                for j in range(n_inputs):
+                    layer.vdW[i, j] = Decimal(self.beta1) * Decimal(layer.vdW[i, j]) + Decimal(1 - self.beta1) * layer.W[i, j].gradient
+                    layer.sdW[i, j] = Decimal(self.beta2) * Decimal(layer.sdW[i, j]) + Decimal(1 - self.beta2) * layer.W[i, j].gradient ** 2
+                    layer.vdW_corrected[i, j] = Decimal(layer.vdW[i, j]) / Decimal(1 - self.beta1 ** (t + 1))
+                    layer.sdW_corrected[i, j] = Decimal(layer.sdW[i, j]) / Decimal(1 - self.beta2 ** (t + 1))
+
+                    layer.W[i, j].data -= Decimal(self.learning_rate) * Decimal(layer.vdW_corrected[i, j]) / (Decimal(layer.sdW_corrected[i, j]) + Decimal(AdaptiveMomentEstimation.EPSILON)).sqrt()
+            
+            # updating vdb
+            n_units, n_inputs = layer.vdb.shape
+            for i in range(n_units):
+                for j in range(n_inputs):
+                    layer.vdb[i, j] = Decimal(self.beta1) * Decimal(layer.vdb[i, j]) + Decimal(1 - self.beta1) * layer.b[i, j].gradient
+                    layer.sdb[i, j] = Decimal(self.beta2) * Decimal(layer.sdb[i, j]) + Decimal(1 - self.beta2) * layer.b[i, j].gradient ** 2
+                    layer.vdb_corrected[i, j] = Decimal(layer.vdb[i, j]) / Decimal(1 - self.beta1 ** (t + 1))
+                    layer.sdb_corrected[i, j] = Decimal(layer.sdb[i, j]) / Decimal(1 - self.beta2 ** (t + 1))
+
+                    layer.b[i, j].data -= Decimal(self.learning_rate) * Decimal(layer.vdb_corrected[i, j]) / (Decimal(layer.sdb_corrected[i, j]) + Decimal(AdaptiveMomentEstimation.EPSILON)).sqrt()
+
+    def __repr__(self) -> str:
+        return f"AdaptiveMomentEstimation(batch_size={self.batch_size}, learning_rate={self.learning_rate0}, beta1={self.beta1}, beta2={self.beta2})"
