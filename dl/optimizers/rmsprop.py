e33d8af47ff93a0c6f9dae211dc0ede0d73c1f1f
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2023-01-05 17:44:02 +0100

    ADD: implementing RMSProp

--- dl/optimizers/rmsprop.py
+++ dl/optimizers/rmsprop.py
@@ -1,7 +1,11 @@
 import numpy as np
+import fractions
+from decimal import Decimal
 
+from dl.layers.dense import Dense
+from dl.optimizers.optimizer import Optimizer
 
-class RMSProp:
+class RMSProp(Optimizer):
     EPSILON = 1e-7
 
     def __init__(self, batch_size, learning_rate, lr_decay=lambda lr0, epoch: lr0, beta=0.999):
@@ -12,63 +16,76 @@ class RMSProp:
         self.beta = beta
 
     def initialize(self):
-        for l in range(1, self.model.L):
-            # velocity
-            self.model.layers[l].sdW = np.zeros((self.model.layers[l].n_units, self.model.layers[l].n_inputs))
-            self.model.layers[l].sdb = np.zeros((self.model.layers[l].n_units, 1))
+        for layer in self.model.layers:
+            if isinstance(layer, Dense):
+                layer.sdW = np.zeros(layer.W.shape)
+                layer.sdb = np.zeros(layer.b.shape)
 
     def __call__(self, X, y, n_epochs, verbose=True):
         history = {"loss": []}
-        n, m = X.shape
+
+        m, n = X.shape
         number_batches = np.ceil(m / self.batch_size).astype(int)
 
         for epoch in range(n_epochs):
             # applying learning rate decay
             self.learning_rate = self.lr_decay(self.learning_rate0, epoch)
 
+            average_loss = 0
             for t in range(number_batches):
                 starting_index = t * self.batch_size
                 finishing_index = starting_index + self.batch_size
 
                 finishing_index = min(finishing_index, m)
 
-                X_t = X[:, starting_index:finishing_index]
-                y_t = y[:, starting_index:finishing_index]
+                X_t = X[starting_index:finishing_index, :]
+                y_t = y[starting_index:finishing_index, :]
 
                 # forward propagation
-                self.model.layers[0].A = X_t
-                self.model.forward_propagation()
+                y_pred = self.model(X_t, is_optimizing=True)
 
                 # loss calculation
-                y_hat = self.model.layers[-1].A
-                loss = self.model.loss(y_hat, y_t)
+                loss = self.model.loss(y_pred, y_t)
 
-                if self.model.loss.regularizer is not None:
-                    for l in range(1, self.model.L):
-                        loss += self.model.loss.regularizer(m, self.model.layers[l].W)
+                # model params
+                params = self.model.parameters()
 
                 # backward propagation - computing dWi & dbi for every layer
-                self.model.layers[-1].dA = self.model.loss.prime(y_hat, y_t)
-                self.model.backward_propagation(m)
+                self.zero_gradients(params)
+                loss.backward()
 
                 # updating dWi & dbi
-                for l in range(1, self.model.L):
-                    # velocity update
-                    self.model.layers[l].sdW = self.beta * self.model.layers[l].sdW + \
-                        (1 - self.beta) * self.model.layers[l].dW ** 2
-                    self.model.layers[l].sdb = self.beta * self.model.layers[l].sdb + \
-                        (1 - self.beta) * self.model.layers[l].db ** 2
-
-                    self.model.layers[l].W -= self.learning_rate * self.model.layers[l].dW / \
-                        (np.sqrt(self.model.layers[l].sdW) + RMSProp.EPSILON)
-                    self.model.layers[l].b -= self.learning_rate * self.model.layers[l].db / \
-                        (np.sqrt(self.model.layers[l].sdb) + RMSProp.EPSILON)
+                self.step()
 
                 # printing to the console
                 if verbose:
-                    print(f"[{epoch + 1}/{n_epochs}, {t + 1}/{number_batches}]: loss = {loss}")
+                    print(f"[{epoch + 1}/{n_epochs}, {t + 1}/{number_batches}]: loss = {loss.data}")
+                
+                average_loss += loss.data
 
             # keeping history
-            history["loss"].append(loss)
+            history["loss"].append(average_loss / number_batches)
 
         return history
+    
+    def step(self):
+        for layer in self.model.layers:
+            if not isinstance(layer, Dense):
+                continue
+            
+            # updating sdW
+            n_units, n_inputs = layer.sdW.shape
+            for i in range(n_units):
+                for j in range(n_inputs):
+                    layer.sdW[i, j] = Decimal(self.beta) * Decimal(layer.sdW[i, j]) + Decimal(1 - self.beta) * layer.W[i, j].gradient ** 2
+                    layer.W[i, j].data -= Decimal(self.learning_rate) * layer.W[i, j].gradient / (Decimal(layer.sdW[i, j]).sqrt() + Decimal(RMSProp.EPSILON))
+            
+            # updating sdb
+            n_units, n_inputs = layer.sdb.shape
+            for i in range(n_units):
+                for j in range(n_inputs):
+                    layer.sdb[i, j] = Decimal(self.beta) * Decimal(layer.sdb[i, j]) + Decimal(1 - self.beta) * layer.b[i, j].gradient ** 2
+                    layer.b[i, j].data -= Decimal(self.learning_rate) * layer.b[i, j].gradient / (Decimal(layer.sdb[i, j]).sqrt() + Decimal(RMSProp.EPSILON))
+
+    def __repr__(self) -> str:
+        return f"RMSProp(batch_size={self.batch_size}, learning_rate={self.learning_rate0}, beta={self.beta})"
