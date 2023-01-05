6bb104ff2a080eca62276692fbecea209cc58352
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2023-01-05 17:15:50 +0100

    ADD: implementing Gradient Descent with Momentum

--- dl/optimizers/momentum.py
+++ dl/optimizers/momentum.py
@@ -1,7 +1,11 @@
 import numpy as np
+from decimal import Decimal
 
+from dl.layers.dense import Dense
+from dl.optimizers.optimizer import Optimizer
+from dl.automatic_gradient.variable import Variable
 
-class MomentumGradientDescent:
+class MomentumGradientDescent(Optimizer):
     def __init__(self, batch_size, learning_rate, lr_decay=lambda lr0, epoch: lr0, beta=0.9):
         self.batch_size = batch_size
         self.learning_rate0 = learning_rate
@@ -10,61 +14,76 @@ class MomentumGradientDescent:
         self.beta = beta
 
     def initialize(self):
-        for l in range(1, self.model.L):
-            # velocity
-            self.model.layers[l].vdW = np.zeros((self.model.layers[l].n_units, self.model.layers[l].n_inputs))
-            self.model.layers[l].vdb = np.zeros((self.model.layers[l].n_units, 1))
+        for layer in self.model.layers:
+            if isinstance(layer, Dense):
+                layer.vdW = np.zeros(layer.W.shape)
+                layer.vdb = np.zeros(layer.b.shape)
 
     def __call__(self, X, y, n_epochs, verbose=True):
         history = {"loss": []}
-        n, m = X.shape
+
+        m, n = X.shape
         number_batches = np.ceil(m / self.batch_size).astype(int)
 
         for epoch in range(n_epochs):
             # applying learning rate decay
             self.learning_rate = self.lr_decay(self.learning_rate0, epoch)
 
+            average_loss = 0
             for t in range(number_batches):
                 starting_index = t * self.batch_size
                 finishing_index = starting_index + self.batch_size
 
                 finishing_index = min(finishing_index, m)
 
-                X_t = X[:, starting_index:finishing_index]
-                y_t = y[:, starting_index:finishing_index]
+                X_t = X[starting_index:finishing_index, :]
+                y_t = y[starting_index:finishing_index, :]
 
                 # forward propagation
-                self.model.layers[0].A = X_t
-                self.model.forward_propagation()
+                y_pred = self.model(X_t, is_optimizing=True)
 
                 # loss calculation
-                y_hat = self.model.layers[-1].A
-                loss = self.model.loss(y_hat, y_t)
+                loss = self.model.loss(y_pred, y_t)
 
-                if self.model.loss.regularizer is not None:
-                    for l in range(1, self.model.L):
-                        loss += self.model.loss.regularizer(m, self.model.layers[l].W)
+                # model params
+                params = self.model.parameters()
 
                 # backward propagation - computing dWi & dbi for every layer
-                self.model.layers[-1].dA = self.model.loss.prime(y_hat, y_t)
-                self.model.backward_propagation(m)
+                self.zero_gradients(params)
+                loss.backward()
 
                 # updating dWi & dbi
-                for l in range(1, self.model.L):
-                    # velocity update
-                    self.model.layers[l].vdW = self.beta * self.model.layers[l].vdW + \
-                        (1 - self.beta) * self.model.layers[l].dW
-                    self.model.layers[l].vdb = self.beta * self.model.layers[l].vdb + \
-                        (1 - self.beta) * self.model.layers[l].db
-
-                    self.model.layers[l].W -= self.learning_rate * self.model.layers[l].vdW
-                    self.model.layers[l].b -= self.learning_rate * self.model.layers[l].vdb
+                self.step()
 
                 # printing to the console
                 if verbose:
-                    print(f"[{epoch + 1}/{n_epochs}, {t + 1}/{number_batches}]: loss = {loss}")
+                    print(f"[{epoch + 1}/{n_epochs}, {t + 1}/{number_batches}]: loss = {loss.data}")
+                
+                average_loss += loss.data
 
             # keeping history
-            history["loss"].append(loss)
+            history["loss"].append(average_loss / number_batches)
 
         return history
+
+    def step(self):
+        for layer in self.model.layers:
+            if not isinstance(layer, Dense):
+                continue
+            
+            # updating vdW
+            n_units, n_inputs = layer.vdW.shape
+            for i in range(n_units):
+                for j in range(n_inputs):
+                    layer.vdW[i, j] = Decimal(self.beta) * Decimal(layer.vdW[i, j]) + Decimal(1 - self.beta) * layer.W[i, j].gradient
+                    layer.W[i, j].data -= Decimal(self.learning_rate) * Decimal(layer.vdW[i, j])
+            
+            # updating vdb
+            n_units, n_inputs = layer.vdb.shape
+            for i in range(n_units):
+                for j in range(n_inputs):
+                    layer.vdb[i, j] = Decimal(self.beta) * Decimal(layer.vdb[i, j]) + Decimal(1 - self.beta) * layer.b[i, j].gradient
+                    layer.b[i, j].data -= Decimal(self.learning_rate) * Decimal(layer.vdb[i, j])
+
+    def __repr__(self) -> str:
+        return f"MomentumGradientDescent(batch_size={self.batch_size}, learning_rate={self.learning_rate0}, beta={self.beta})"
