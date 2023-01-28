5c28dc0ffbf27046e8989911a030481c71b139da
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2023-01-28 12:24:48 +0100

    updating init of initializers

---
+++ dl/initializers/__init__.py
@@ -0,0 +1,6 @@
+from dl.initializers.he_normal import HeNormal
+from dl.initializers.he_uniform import HeUniform
+from dl.initializers.random_normal import RandomNormal
+from dl.initializers.random_uniform import RandomUniform
+from dl.initializers.xavier_normal import XavierNormal
+from dl.initializers.xavier_uniform import XavierUniform
