5df1869d772c6b8c6ce31280f08def1eb434ed76
Mohamed Fouad HANANI <mohamedfouad.hanani@gmail.com>
2023-01-31 12:31:07 +0100

    update categorical cross entropy loss

--- dl/losses/cce.py
+++ dl/losses/cce.py
@@ -6,10 +6,7 @@ class CategoricalCrossEntropy(Loss):
         pass
     
     def forward(self, predictions, actuals):
-        exps = np.exp(predictions - np.amax(predictions, axis=1, keepdims=True))
-        softmax = exps / exps.sum(axis=1, keepdims=True)
-        predictions = np.clip(softmax, 1e-15, 1 - 1e-15)
-        
+        predictions = np.clip(predictions, 1e-15, 1 - 1e-15)
         log_loss = np.log(np.sum(predictions * actuals, axis=1))
 
         return -np.mean(log_loss)
